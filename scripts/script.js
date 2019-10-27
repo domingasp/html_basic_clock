9b82dd46477fbed031595cb4eafce3d76bb3eead
Domingas <domingas@Domingass-MacBook-Pro.local>
2019-10-27 22:36:41 +0000

    Added an interval to script.js which updates the clock every second.

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,7 @@
 window.setInterval(function() {
-    
+    var time = new Date();
+
+    var currentTimeLabel = document.getElementById("currentTimeP");
+    currentTimeLabel.innerHTML = time.toLocaleTimeString()
+
 }, 1000);
